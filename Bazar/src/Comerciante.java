7611fe0f1330a608df87f15e8ae28e1fae0af3eb
YahirSarabia <158230448+YahirSarabia@users.noreply.github.com>
2024-03-11 15:14:11 -0700

    Bazar
    Relación entre las clases

---
+++ Bazar/src/Comerciante.java
@@ -0,0 +1,17 @@
+public class Comerciante {
+    private int id_comerciante;
+    private String nombre;
+    private String email;
+    private String contrasena;
+
+    public Comerciante(){
+    }
+
+    public Comerciante(int id_comerciante, String nombre, String email, String contrasena){
+        this.id_comerciante = id_comerciante;
+        this.nombre = nombre;
+        this.email = email;
+        this.contrasena = contrasena;
+    }
+
+}
---
+++ Bazar/src/Producto.java
@@ -0,0 +1,58 @@
+public class Producto {
+    private int id_producto;
+    private String nombre;
+    private String descripcion;
+    private float precio;
+    private int cantidad;
+
+    public Producto(){
+    }
+
+    public Producto(int id_producto, String nombre, String descripcion, float precio, int cantidad){
+        this.id_producto = id_producto;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.cantidad = cantidad;
+    }
+
+    public int getId_producto() {
+        return id_producto;
+    }
+
+    public void setId_producto(int id_producto) {
+        this.id_producto = id_producto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+}
